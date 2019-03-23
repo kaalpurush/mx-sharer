eee40d1d60dfd110421e555769b657ee7726122d
Ariful Islam <ariful@portonics.com>
2019-03-23 14:20:03 +0600

    support for free version

--- app/src/main/java/com/codelixir/mxsharer/MainActivity.java
+++ app/src/main/java/com/codelixir/mxsharer/MainActivity.java
@@ -1,9 +1,13 @@
 package com.codelixir.mxsharer;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.TextView;
@@ -14,6 +18,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Uri uri;
+
+    private final int OPEN_PLAYER_REQUEST = 786;
+
+    long back_pressed = 0;
+
+    boolean confirmBeforeExit=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,13 +106,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Uri openMXPlayer(Uri uri) {
+        confirmBeforeExit=true;
         Intent i = new Intent(Intent.ACTION_VIEW);
         Uri videoUri = uri;
         i.setDataAndType(videoUri, "video/*");
         i.setPackage("com.mxtech.videoplayer.pro");
-        if (i.resolveActivity(getPackageManager()) != null)
-            startActivity(i);
+        if (isIntentValid(this, i)) {
+            startActivityForResult(i, 786);
+        } else {
+            i.setPackage("com.mxtech.videoplayer.ad");
+            if (isIntentValid(this, i))
+                startActivityForResult(i, 786);
+        }
 
         return uri;
+    }
+
+    static boolean isIntentValid(Context context, Intent intent) {
+        PackageManager packageManager = context.getPackageManager();
+        return intent.resolveActivity(packageManager) != null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == OPEN_PLAYER_REQUEST) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    if (data.hasExtra("end_by") && data.getStringExtra("end_by").equals("playback_completion"))
+                        Toast.makeText(this, R.string.playback_end, Toast.LENGTH_SHORT);
+                    else
+                        Toast.makeText(this, R.string.playback_closed, Toast.LENGTH_SHORT);
+                    confirmBeforeExit=false;
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Toast.makeText(this, R.string.playback_calcelled, Toast.LENGTH_SHORT);
+                    break;
+                case Activity.RESULT_FIRST_USER:
+                    Toast.makeText(this, R.string.playback_error, Toast.LENGTH_SHORT);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!confirmBeforeExit || back_pressed + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(getBaseContext(),
+                    getResources().getString(R.string.press_back), Toast.LENGTH_SHORT)
+                    .show();
+        }
+        back_pressed = System.currentTimeMillis();
     }
 }
